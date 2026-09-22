da7e313a9e58d473ef9a03f4b49f7fbdc5f0df50
shamilkazimlidably-maker <shamilkazimli.dably@gmail.com>
2026-09-22 21:28:11 +0500

    Delete borabet 3 directory

--- borabet 3/components/AppContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext, useContext } from 'react';
-export const AppCtx = createContext(null);
-export const useApp = () => useContext(AppCtx);
--- borabet 3/pages/api/notifications.js
+++
@@ -1,8 +0,0 @@
-import { handler, db, q } from '../../lib/server/db';
-import { authPlayer } from '../../lib/server/auth';
-
-export default handler(['GET'], async (req) => {
-  const p = await authPlayer(req);
-  const rows = await q(db().from('notifications').select('*').eq('player_id', p.id).order('created_at', { ascending: false }).limit(60));
-  return { items: rows };
-});
